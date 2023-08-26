3d530b4af78ac72dde0efef1167aebe27644b0e3
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-08-27 04:17:00 +0930

    Update Finder.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -10,7 +10,7 @@ using namespace std;
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
     std::vector<int> result;
     for (size_t i = 0; i < s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0,i+1), found-1);
+        size_t found = s1.find(s2.substr(0,i+1), found-i);
         if (found != std::string::npos) {
             result.push_back(found);
         } else {
