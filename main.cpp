65b8491ae6c8c9126702397d3b258121384e88a6
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-09-04 22:19:08 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,39 +6,45 @@
 
 int main() {
     // Create an unsorted list of integers
-    std::vector<int> unsortedList = {5, 2, 9, 1, 5, 3};
-
-    // Sort the list using BubbleSort
-    BubbleSort bubbleSort;
-    std::vector<int> sortedBubble = bubbleSort.sort(unsortedList);
+    std::vector<int> unsortedListA = {1, 3, 5, 4, -5, 100, 7777, 2014, 0};
 
     // Sort the list using QuickSort
     QuickSort quickSort;
-    std::vector<int> sortedQuick = quickSort.sort(unsortedList);
+    std::vector<int> sortedQuick = quickSort.sort(unsortedListA);
 
-    // Print the sorted lists
-    std::cout << "Bubble Sort Result:" << std::endl;
-    for (int num : sortedBubble) {
-        std::cout << num << " ";
+    // Perform a binary search on the sorted list
+    RecursiveBinarySearch binarySearch;
+    int target = 1; // Change this to the element you want to search for
+    bool found = binarySearch.search(sortedQuick, target);
+
+    if (found==1) {
+        std::cout << "true ";
+    } else {
+        std::cout << "Element " << target << " not found in the sorted list." << std::endl;
     }
-    std::cout << std::endl;
 
-    std::cout << "Quick Sort Result:" << std::endl;
     for (int num : sortedQuick) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
 
-    // Perform a binary search on the sorted list
-    RecursiveBinarySearch binarySearch;
-    int target = 5; // Change this to the element you want to search for
-    bool found = binarySearch.search(sortedQuick, target);
+    // Sort the list using BubbleSort
+    BubbleSort bubbleSort;
+    std::vector<int> sortedBubble = bubbleSort.sort(unsortedListA);
 
-    if (found) {
-        std::cout << "Element " << target << " found in the sorted list." << std::endl;
-    } else {
-        std::cout << "Element " << target << " not found in the sorted list." << std::endl;
+
+
+
+    for (int num : sortedBubble) {
+        std::cout << num << " ";
     }
+    std::cout << std::endl;
+
+
+
+
+
+
 
     return 0;
 }
