b803ef6ff237d53d8f2c52d60184fb07a5a57642
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-09-11 03:42:11 +0930

    Update BigNumCalc.h

--- BigNumCalc.h
+++ BigNumCalc.h
@@ -9,7 +9,7 @@ public:
     std::list<int> buildBigNum(const std::string& numString);
     std::list<int> add(const std::list<int>& num1, const std::list<int>& num2);
     std::list<int> sub(const std::list<int>& num1, const std::list<int>& num2);
-    std::list<int> mul(const std::list<int>& num1, int num2);
+    std::list<int> mul(const std::list<int>& num1, const std::list<int>& num2);
 
 private:
     void removeLeadingZeros(std::list<int>& num);
