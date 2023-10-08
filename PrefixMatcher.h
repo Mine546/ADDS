2039c1c4723afced8f7d079c4977b21ec28a5a35
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-10-08 16:57:00 +1030

    Update PrefixMatcher.h

--- PrefixMatcher.h
+++ PrefixMatcher.h
@@ -1,16 +1,12 @@
-// PrefixMatcher.h
 #ifndef PREFIXMATCHER_H
 #define PREFIXMATCHER_H
-
 #include "Trie.h"
-
 class PrefixMatcher {
 private:
     Trie trie;
-
 public:
     void insert(const std::string& address, int routerNumber);
     int selectRouter(const std::string& networkAddress);
 };
 
-#endif // PREFIXMATCHER_H
+#endif
