067116eaada0949a0f74fdcb85c7741d906b1215
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-10-08 16:56:42 +1030

    Update Autocomplete.h

--- Autocomplete.h
+++ Autocomplete.h
@@ -1,16 +1,12 @@
-// Autocomplete.h
 #ifndef AUTOCOMPLETE_H
 #define AUTOCOMPLETE_H
-
 #include "Trie.h"
-
 class Autocomplete {
 private:
     Trie trie;
-
 public:
     void insert(const std::string& word);
     std::vector<std::string> getSuggestions(const std::string& partialWord);
 };
 
-#endif // AUTOCOMPLETE_H
+#endif
