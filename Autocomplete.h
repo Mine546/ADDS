9f60d931766b4071a7ef1c3b51d8ba16822c36dd
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-10-08 16:52:55 +1030

    Add files via upload

---
+++ Autocomplete.h
@@ -0,0 +1,16 @@
+// Autocomplete.h
+#ifndef AUTOCOMPLETE_H
+#define AUTOCOMPLETE_H
+
+#include "Trie.h"
+
+class Autocomplete {
+private:
+    Trie trie;
+
+public:
+    void insert(const std::string& word);
+    std::vector<std::string> getSuggestions(const std::string& partialWord);
+};
+
+#endif // AUTOCOMPLETE_H
