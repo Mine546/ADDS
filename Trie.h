a9c5ff5c103ae60d4dcdc6b5ea952166ded4f0fb
Mine546 <105634840+Mine546@users.noreply.github.com>
2023-10-08 16:55:35 +1030

    Update Trie.h

--- Trie.h
+++ Trie.h
@@ -1,7 +1,5 @@
-// Trie.h
 #ifndef TRIE_H
 #define TRIE_H
-
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -10,21 +8,18 @@ class TrieNode {
 public:
     std::unordered_map<char, TrieNode*> children;
     bool isEndOfWord;
-
     TrieNode();
 };
 
 class Trie {
 private:
     TrieNode* root;
-
 public:
     Trie();
     void insert(const std::string& word);
     std::vector<std::string> getSuggestions(const std::string& partialWord);
-
 private:
     void getAllWordsWithPrefix(TrieNode* node, std::string prefix, std::vector<std::string>& suggestions);
 };
 
-#endif // TRIE_H
+#endif
